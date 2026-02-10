19c25348a39da53fc214a3717282c5b9348ec40e
“jaimesnh” <jaimesv1203@gmail.com>
2026-02-10 10:01:58 +0100

    Spring project migration

---
+++ scam-g18/src/main/java/es/codeurjc/scam_g18/ScamG18Application.java
@@ -0,0 +1,13 @@
+package es.codeurjc.scam_g18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScamG18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScamG18Application.class, args);
+	}
+
+}
---
+++ scam-g18/src/test/java/es/codeurjc/scam_g18/ScamG18ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.scam_g18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScamG18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
