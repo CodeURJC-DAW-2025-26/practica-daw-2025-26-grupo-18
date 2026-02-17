94ac616adb805b8b2916dbd8545f0f306e488c1f
albertohontanilla <a.hontanilla.2023@alumnos.urjc.es>
2026-02-17 11:46:59 +0100

    Remove duplicate import of List in CourseRepository

--- scam-g18/src/main/java/es/codeurjc/scam_g18/repository/CourseRepository.java
+++ scam-g18/src/main/java/es/codeurjc/scam_g18/repository/CourseRepository.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.domain.Pageable; // Correct import
-import java.util.List; // Ensure List is imported
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
